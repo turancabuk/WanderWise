1a49506b81f601884ea83be87282a1d9d1d7e527
turan cabuk <turan6677@gmail.com>
2024-07-14 13:08:55 +0300

    Initial Commit

---
+++ WanderWise/WanderWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderWiseApp.swift
+//  WanderWise
+//
+//  Created by Turan Çabuk on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WanderWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
