17cc9b3a9020cf98ce20ba23df2420dcdd22b6da
turan cabuk <turan6677@gmail.com>
2024-07-14 13:17:55 +0300

    project has been started.

--- WanderWise/ContentView.swift
+++ WanderWise/ContentView.swift
@@ -13,9 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, test!")
         }
-        .padding()
     }
 }
 
